a2939cb4813454ff2956ba786bc69a7e625f2944
Vinit-Pothuraju <vinitkumarpothuraju@gmail.com>
2025-05-06 11:50:23 +0530

    client setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from './components/ui/button'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button>Shadcn</Button>
+    </>
+  )
+}
+
+export default App
